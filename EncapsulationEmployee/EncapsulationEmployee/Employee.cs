e39cc0d1bb826112858660f2c0570cf416ab6d0c
Edmon Grigoryan <edmongrigoryan@outlook.com>
2018-11-09 00:32:16 +0400

    class Employee

---
+++ EncapsulationEmployee/EncapsulationEmployee/Employee.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EncapsulationEmployee
+{
+    class Employee
+    {
+        private string empName;
+        private int empId;
+        private float curPay;
+
+        public Employee(string name,int id, float pay)
+        {
+            EmpName = name;
+            EmpId = id;
+            CurPay = pay;
+        }
+
+        public string EmpName
+        {
+            get { return empName; }
+            set
+            {
+                if(value.Length>15)
+                {
+                    Console.WriteLine("Error! Name length exceeds 15 characters!");
+                }
+                empName = value;
+            }
+
+        }
+
+        public int EmpId
+        {
+            get { return empId; }
+            set { empId = value; }
+        }
+
+        public float CurPay
+        {
+            get { return curPay; }
+            set { curPay = value; }
+        }
+
+        public void GiveBonus(int amount)
+        {
+            CurPay += amount;
+        }
+    }
+}
---
+++ EncapsulationEmployee/EncapsulationEmployee/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EncapsulationEmployee
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee emp = new Employee("Ashot", 10, 4500);
+            emp.GiveBonus(1000);
+            Console.WriteLine($"emp curpay  is {emp.CurPay}");
+
+            emp.EmpName = "Karen";
+            Console.WriteLine($"emp name is {emp.EmpName}");
+        }
+    }
+}
