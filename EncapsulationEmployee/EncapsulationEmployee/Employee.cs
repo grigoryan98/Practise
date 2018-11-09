4fa3020eb5eb03b27eb5dad90abe63ef85844212
ISTC CREATE LAB PC <ISTC CREATE LAB PC@ISTC-CREATE-LAB>
2018-11-09 12:21:45 +0400

    This, DispalyNotes():

--- EncapsulationEmployee/EncapsulationEmployee/Employee.cs
+++ EncapsulationEmployee/EncapsulationEmployee/Employee.cs
@@ -8,15 +8,32 @@ namespace EncapsulationEmployee
 {
     class Employee
     {
+        private int empAge;
         private string empName;
         private int empId;
         private float curPay;
 
-        public Employee(string name,int id, float pay)
+        public Employee(string name , int id, float pay):this(name,0,id,pay)
+        {
+
+        }
+
+
+        public Employee(string name, int age, int id, float pay)
         {
             EmpName = name;
+            Age = age;
             EmpId = id;
             CurPay = pay;
+        }
+
+
+
+
+        public int Age
+        {
+            get { return empAge; }
+            set { empAge = value; }
         }
 
         public string EmpName
@@ -48,6 +65,14 @@ namespace EncapsulationEmployee
         public void GiveBonus(int amount)
         {
             CurPay += amount;
+        }
+
+        public void DisplayNotes()
+        {
+            Console.WriteLine($"Employee name is {EmpName}");
+            Console.WriteLine($"Employee age is {Age }");
+            Console.WriteLine($"Employee id is {EmpId}");
+            Console.WriteLine($"Employee CurrPay is {CurPay}");
         }
     }
 }
