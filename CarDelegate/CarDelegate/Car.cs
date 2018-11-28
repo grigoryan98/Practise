d5e06554880cc3d6f8446a6c5001bf32f3c48f62
ISTC CREATE LAB PC <ISTC CREATE LAB PC@ISTC-CREATE-LAB>
2018-11-28 16:04:52 +0400

    simple delegate

---
+++ CarDelegate/CarDelegate/Car.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarDelegate
+{
+    class Car
+    {
+        public  int CurrSpeed { get; set; }
+        public int MaxSpeed { get; set; }
+        public string PetName { get; set; }
+
+        private bool carIsDead;
+
+        public Car()
+        {
+
+        }
+
+        public Car(int cs, int max, string name)
+        {
+            CurrSpeed = cs;
+            MaxSpeed = max;
+            PetName = name;
+        }
+
+        public delegate void CarEngineHandler(string msgForCaller);
+        private CarEngineHandler listOfHandlers;
+        public void RegistreWithCarEngine(CarEngineHandler methodToCall)
+        {
+            listOfHandlers = methodToCall;
+        }
+
+        public void Accelerate(int delta)
+        {
+            if (carIsDead)
+            {
+                if (listOfHandlers != null)
+                    listOfHandlers("Sorry: this car is dead...");
+            }
+            else
+            {
+                CurrSpeed += delta;
+
+                //is this car almost dead
+                if (10 == MaxSpeed - CurrSpeed && listOfHandlers != null)
+                {
+                    listOfHandlers("Careful boddy gonna below");
+                }
+                if (CurrSpeed > MaxSpeed)
+                    carIsDead = true;
+                else
+                    Console.WriteLine("CurrentSpeed = {0}", CurrSpeed);
+            }
+        }
+    }
+
+}
---
+++ CarDelegate/CarDelegate/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarDelegate
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //create class instance 
+            Car c1 = new Car(10, 100, "BMW");
+
+            c1.RegistreWithCarEngine(OnCarEngineHandler);
+
+            Console.WriteLine("-----Speeding up---------");
+            for(int i=0;i<6;i++)
+            {
+                c1.Accelerate(20);
+            }
+
+        }
+        public static void OnCarEngineHandler(string msg)
+        {
+            Console.WriteLine("\n***** Message From Car Object *****");
+            Console.WriteLine(msg);
+            Console.WriteLine("----------------\n");
+        }
+    }
+}
