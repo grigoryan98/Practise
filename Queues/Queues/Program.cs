810e936b7f5ce1e362b258246a7c31ea2a74fe7c
edmon.grigoryan <edmon.grigoryan@CREATE-1-2>
2019-01-30 18:55:06 +0400

    Queue Dequeue Enqueue

---
+++ Queues/Queues/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Queues
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            QueueDemo qd = new QueueDemo();
+            Queue q = new Queue();
+
+            foreach (int i in q)
+                Console.Write(i + " ");
+
+            qd.QueueEnqueue(q, 7);
+            qd.QueueEnqueue(q, 10);
+            qd.QueueEnqueue(q, 78);
+            qd.QueueDequeue(q);
+            qd.QueueDequeue(q);
+            qd.QueueDequeue(q);
+
+            try
+            {
+                qd.QueueDequeue(q);
+            }
+            catch (InvalidOperationException)
+            {
+                Console.WriteLine("Queue empty.");
+            }
+
+        }
+    }
+}
---
+++ Queues/Queues/QueueDemo.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Queues
+{
+    class QueueDemo
+    {
+        public void QueueEnqueue(Queue q, int a)
+        {
+            q.Enqueue(a);
+            Console.WriteLine($"Enque {a}");
+            // Contain
+            foreach(int c in q)
+                Console.Write(c+" ");
+            Console.WriteLine();
+        }
+        public void QueueDequeue(Queue q)
+        {
+            Console.WriteLine("Queue Dequeue");
+            int a = (int)q.Dequeue();
+            Console.WriteLine(q);
+
+            Console.WriteLine(q);
+            foreach(int c in q)
+                Console.Write(q);
+            Console.WriteLine();
+        }
+    }
+}
