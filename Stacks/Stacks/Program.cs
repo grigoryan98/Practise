c42d1ede2a6c93346a5699964e9b690812f7371c
edmon.grigoryan <edmon.grigoryan@CREATE-1-2>
2019-01-30 16:58:41 +0400

    Stack Push Pop

---
+++ Stacks/Stacks/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Stacks
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            StackDemo sd = new StackDemo();
+            Stack st = new Stack();
+
+            foreach (int c in st)
+                Console.WriteLine(c + " ");
+
+            StackDemo.StackPush(st, 17);
+            StackDemo.StackPush(st, 78);
+            StackDemo.StackPush(st, 24);
+            sd.StackPop(st);
+            sd.StackPop(st);
+            sd.StackPop(st);
+
+        }
+    }
+}
---
+++ Stacks/Stacks/StackDemo.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace Stacks
+{
+    public class StackDemo
+    {
+        public static void StackPush(Stack st, int a)
+        {
+            st.Push(a);
+            Console.WriteLine($"Push {a}");
+
+            Console.WriteLine("Stack ");
+            foreach (int c in st)
+                Console.Write(c + " ");
+            Console.WriteLine();
+        }
+        public  void StackPop(Stack st)
+        {
+            Console.WriteLine("Stack Pop");
+            int a = (int)st.Pop();
+            Console.WriteLine(a);
+
+            Console.WriteLine("Stack");
+            foreach (int c in st)
+                Console.Write(c + " ");
+            Console.WriteLine();
+
+        }
+    }
+}
