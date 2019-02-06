501c437ce40da85c3efc80abff6d129a80da5dae
grigoryan98 <edmon.grigoryan23@gmail.com>
2019-02-06 11:23:01 +0400

    Working whit text IFormattable

---
+++ WorkingWhitIFormatable/WorkingWhitIFormatable/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Globalization;
+
+namespace WorkingWhitIFormatProvider
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Temperature temp = new Temperature(13);
+            Console.WriteLine("Temperature [default]={0}",temp);
+            Console.WriteLine("Temperature [Fahrenheit]={0:F}",temp);
+            Console.WriteLine("Temperature [CultureInfo]={0}",temp.ToString("F",CultureInfo.CreateSpecificCulture("en-US")));
+            Console.WriteLine("temperature [CultureInfo]={0}",temp.ToString("C",CultureInfo.CreateSpecificCulture("ru-RU")));
+            Console.ReadKey();
+        }
+    }
+}
---
+++ WorkingWhitIFormatable/WorkingWhitIFormatable/Temperature.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Globalization;
+
+namespace WorkingWhitIFormatProvider
+{
+    class Temperature : IFormattable
+    {
+        public decimal temperature;
+
+        public Temperature(int temperature)
+        {
+            if (temperature < -273.15m)
+                throw new ArgumentOutOfRangeException("temperature can't be smaler than -273.15");
+            this.temperature = temperature;
+        }
+
+        public decimal Celsius
+        {
+            get { return temperature; }
+        }
+
+        public decimal Fahrenheit
+        {
+            get { return temperature * 9 / 2 + 32; }
+        }
+
+
+
+        public decimal Kelvin
+        {
+            get { return temperature + 273.15m; }
+        }
+
+        //public override string ToString()
+        //{
+        //    return this.ToString("G", CultureInfo.CurrentCulture);
+        //}
+
+        public string ToString(string format, IFormatProvider formatProvider)
+        {
+            if (String.IsNullOrEmpty(format))
+                format = "G";
+            if (formatProvider == null)
+                formatProvider = CultureInfo.CurrentCulture;
+            switch (format.ToUpperInvariant())
+            {
+                case "G":
+                case "C":
+                    return temperature.ToString("F2", formatProvider) + "C";
+                case "F":
+                    return Fahrenheit.ToString("F2", formatProvider) + "F";
+                case "K":
+                    return Kelvin.ToString("F2", formatProvider) + "K";
+                default:
+                    throw
+                         new FormatException(
+                             String.Format("The {0} format  string is  nor suported", formatProvider));
+            }
+        }
+    }
+}
